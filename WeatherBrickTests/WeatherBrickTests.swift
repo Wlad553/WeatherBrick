814227df97baf5d642b0de1db9cc72a3adaf2518
Vladyslav Petrenko <vladyslav533p@icloud.com>
2023-04-14 14:09:10 +0000

    Revert "Merge branch 'tests' into 'task1'"
    This reverts commit 771fd8fe17bc7b1c11c2f3f33ae28c1cfba9d57a

--- WeatherBrickTests/WeatherBrickTests.swift
+++ WeatherBrickTests/WeatherBrickTests.swift
@@ -6,51 +6,26 @@
 import XCTest
 @testable import WeatherBrick
 
-final class WeatherBrickTests: XCTestCase {
-    
-    var viewController: ViewController!
-    
+class WeatherBrickTests: XCTestCase {
     override func setUp() {
         super.setUp()
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        viewController = storyboard.instantiateViewController(withIdentifier: "main") as? ViewController
-        viewController.loadView()
-        viewController.viewDidLoad()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDown() {
-        viewController = nil
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testFakeDataParsing() {
-        let stubbedJSONData = #"{"coord":{"lon":0,"lat":50},"weather":[{"id":801,"main":"Clouds","description":"few clouds","icon":"02d"}],"base":"stations","main":{"temp":9.3,"feels_like":6.37,"temp_min":9.3,"temp_max":9.3,"pressure":1006,"humidity":76,"sea_level":1006,"grnd_level":1006},"visibility":10000,"wind":{"speed":5.95,"deg":250,"gust":8.4},"clouds":{"all":20},"dt":1681404964,"sys":{"country":"FR","sunrise":1681362712,"sunset":1681411736},"timezone":0,"id":3019355,"name":"Étretat","cod":200}"#.data(using: .utf8)
-        
-        guard let data = stubbedJSONData else {
-            XCTFail("No data in stubbedJSONData")
-            return
-        }
-        
-        let parsedData = viewController.networkWeatherManager.parseJSON(withData: data)
-        
-        guard let parsedData = parsedData else {
-            XCTFail("Failed to parse data")
-            return
-        }
-        
-        viewController.updateInterface(with: parsedData)
-        
-        let expectation = expectation(description: "UIElements texts and image update")
-        let result = XCTWaiter.wait(for: [expectation], timeout: 1)
-        
-        if result == XCTWaiter.Result.timedOut {
-            XCTAssertEqual(viewController.cityLabel.text, "Étretat,")
-            XCTAssertEqual(viewController.countryLabel.text, "France")
-            XCTAssertEqual(viewController.temperatureLabel.text, "9")
-            XCTAssertEqual(viewController.weatherConditionLabel.text, "Clouds")
-            XCTAssertEqual(viewController.brickImageView.image, UIImage(named: "image_stone_normal"))
-        } else {
-            XCTFail("Delay interrupted")
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
         }
     }
 }
